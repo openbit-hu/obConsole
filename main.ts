adfad10ff76efc5c41e2fbbd938833552e6a9c14
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2023-10-31 08:33:33 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -2,14 +2,16 @@ let radioGroup = 1  // Default group ID
 let groupLimit = 3  // Maximum number of groups, it is worth keeping it as low as possible to change between projects easily
 let systemMsg = "Open:bit console"  // The first message on the display at startup
 let msgMillis = control.millis()
+let timeLimit = 2000
 radio.setGroup(radioGroup)
 Kitronik_VIEWTEXT32.clearDisplay()
 Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, systemMsg)
 
 input.onButtonPressed(Button.A, function() {
-    radio.setGroup(radioGroup = (++radioGroup) % groupLimit)
+    radio.setGroup(radioGroup = radioGroup % groupLimit + 1)
     Kitronik_VIEWTEXT32.clearDisplay()
     systemMsg = "Radio group : "+ convertToText(radioGroup)
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, systemMsg)
     msgMillis = control.millis()
 })
 
@@ -20,14 +22,4 @@ radio.onReceivedString(function(receivedString: string) {
     if (msg[0] == "DT") Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, msg[1])
     if (msg[0] == "DB") Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, msg[1])
     if (msg[0] == "CD") Kitronik_VIEWTEXT32.clearDisplay()
-})
-
-basic.forever(function() {
-    if(systemMsg!=null){
-        if (control.millis() - msgMillis > 1000) {
-            Kitronik_VIEWTEXT32.clearDisplay()
-            systemMsg=null
-        }
-    }
-    basic.pause(100)
 })
